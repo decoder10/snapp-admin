2dd024f92ef50bace809d4e8461dacbe71028a16
Aram Mkrtchyan <aram.mkrtchyan@betconstruct.com>
2022-11-17 09:27:57 +0400

    clean project

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,10 +3,8 @@ import axios from 'axios';
 window.Promise = Promise;
 
 export default () => {
-  const { REACT_APP_HTTP_PROTO: proto, REACT_APP_WS_HOST: host, REACT_APP_HTTP_PORT: port } = process.env;
-
   const service = axios.create({
-    baseURL: `${proto}://${host}:${port}/`, // url of the api
+    baseURL: ``, // url of the api
     headers: {
       'Content-Type': 'application/json',
     },
