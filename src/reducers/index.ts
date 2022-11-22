de346ca67fc3da568ae4992a09963a7fb193ea85
Tigran <you@example.com>
2022-11-22 10:01:57 +0400

    removed unused code

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import btc from './btc-value';
 import testStore from './test-reducer';
 import testRequestStore from './test-request-reducer';
 
 export const appReducer = combineReducers({
   testRequestStore,
   testStore,
-  btc,
 });
