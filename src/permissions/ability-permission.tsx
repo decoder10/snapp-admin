efcc4cdc11027d86a90a7f48d171af54ea31241f
Aram Mkrtchyan <aram.mkrtchyan@betconstruct.com>
2022-11-23 16:55:26 +0400

    permissions

--- src/permissions/ability-permission.tsx
+++ src/permissions/ability-permission.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 // TODO not finished. should be changed.
 
 export const AbilityPermission: React.FC<IPermissionsProps> = ({ permission, children }) => {
-  return <div>{children}</div>;
+       return <div>{children}</div>;
 };
