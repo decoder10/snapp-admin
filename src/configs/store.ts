2975e0b5b24d449c9659952641acbf6533c512a2
Aram Mkrtchyan <aram.mkrtchyan@betconstruct.com>
2022-11-28 11:11:20 +0400

    added store subscribtion for asuth

--- src/configs/store.ts
+++ src/configs/store.ts
@@ -1,9 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import axios from 'axios';
 import { appReducer } from 'reducers';
 import logger from 'redux-logger';
 
+import { loadAuthState, saveAuthState } from './local-storage';
+
 export const store = configureStore({
   reducer: appReducer,
+  preloadedState: loadAuthState(),
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
+});
+
+store.subscribe(() => {
+  const data = store.getState();
+
+  const loggedInUser = { token: '' };
+
+  saveAuthState(data);
+
+  if (loggedInUser) {
+    axios.defaults.headers.common.Authorization = `Bearer ${loggedInUser.token}`;
+  }
 });
