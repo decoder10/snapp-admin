af41e12743d91d85b8c5ed40a4ef6dcb6937e635
Gor <you@example.com>
2022-12-05 10:52:25 +0400

    sign-in

--- src/ui/sign-in/sign-in.tsx
+++ src/ui/sign-in/sign-in.tsx
@@ -1,21 +1,55 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
 
 import { Button, TextField } from '@mui/material';
 
+interface FormFieldsProps {
+  userName: string;
+  password: string;
+}
+
 const SignIn: FC = () => {
+  const [userData, setUserData] = useState<FormFieldsProps>({
+    userName: '',
+    password: '',
+  });
+
+  const handler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setUserData({ ...userData, ...{ [name]: value } });
+  };
+
+  const onSubmit = (formFields: FormFieldsProps) => {
+    console.log(formFields);
+  };
+
+  const handleSubmit = () => {
+    onSubmit(userData);
+  };
+
   return (
     <section className="signInWrapper">
       <div className="container">
         <h1>Sign In</h1>
-        <TextField label="UserName" variant="outlined" className="input" />
-        <TextField label="Password" variant="outlined" className="input" />
-        <Button
-          variant="contained"
-          onClick={() => {
-            // eslint-disable-next-line no-alert
-            alert('you are login');
-          }}
-        >
+        <TextField
+          name="userName"
+          onChange={handler}
+          value={userData.userName}
+          label="UserName"
+          variant="outlined"
+          className="input"
+          required
+        />
+        <TextField
+          name="password"
+          onChange={handler}
+          value={userData.password}
+          label="Password"
+          variant="outlined"
+          className="input"
+          required
+        />
+        <Button variant="contained" onClick={handleSubmit}>
           Login
         </Button>
       </div>
