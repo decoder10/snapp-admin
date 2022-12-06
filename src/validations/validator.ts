06d3de60583c186d153bca5659fdc5ae513d462a
Gor <you@example.com>
2022-12-06 16:17:43 +0400

    created Validator class

---
+++ src/validations/validator.ts
@@ -0,0 +1,35 @@
+export default class Validator {
+  required(value: string | number): boolean {
+    return !!value;
+  }
+
+  length(value: string, min: number, max: number): boolean {
+    return value.length > min && value.length < max;
+  }
+
+  availability(value: string, symbols: Array<string>): boolean {
+    let count = 0;
+    symbols.forEach(elem => {
+      if (value.includes(elem)) {
+        count = count + 1;
+      }
+    });
+
+    return count === symbols.length;
+  }
+
+  forbidden(value: string, symbols: Array<string>): boolean {
+    let count = 0;
+    symbols.forEach(elem => {
+      if (!value.includes(elem)) {
+        count = count + 1;
+      }
+    });
+    return count === symbols.length;
+  }
+
+  checkPattern(value: string, reg: RegExp): boolean {
+    const regexp = new RegExp(reg);
+    return regexp.test(value);
+  }
+}
