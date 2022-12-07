92fcda614e8ce01070e4183be6a936cd2ec1d1e7
Aram Mkrtchyan <aram.mkrtchyan@betconstruct.com>
2022-12-07 14:02:28 +0400

    ui pages

--- src/configs/store.ts
+++ src/configs/store.ts
@@ -3,17 +3,21 @@ import axios from 'axios';
 import { rootReducer } from 'reducers';
 import logger from 'redux-logger';
 
-import { loadAuthState, saveAuthState } from './local-storage';
+import { loadAuthState, loadLocalStorage, saveAuthState, saveLocalStorage } from './local-storage';
 
 export const store = configureStore({
   reducer: rootReducer,
-  preloadedState: loadAuthState(),
+  preloadedState: { ...loadAuthState(), ...loadLocalStorage('menuState') },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
 store.subscribe(() => {
   const data = store.getState();
+
+  if (data.menuStateStore) {
+    saveLocalStorage('menuState', { menuStateStore: data.menuStateStore });
+  }
 
   if (data.authenticationStore) {
     const loggedInUser = data.authenticationStore.authentication;
