63123bf54a1ac9e4491622000e6663913c49d48e
Aram Mkrtchyan <aram.mkrtchyan@betconstruct.com>
2022-12-21 10:57:00 +0400

    sign in form component

---
+++ src/theme/theme.ts
@@ -0,0 +1,28 @@
+import { createTheme } from '@mui/material';
+
+export const mainLightTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#407aff',
+      light: '#6694FF',
+      dark: '#2C55B2',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#f50057',
+      light: '#F73378',
+      dark: '#AB003C',
+      contrastText: '#fff',
+    },
+    background: {
+      paper: '#fff',
+      default: '#EFF1F2',
+    },
+  },
+});
+
+export const mainDarkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
