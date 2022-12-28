6014b73222495fee4ad570dae09529ff6aaf6ef6
Gor <you@example.com>
2022-12-28 10:55:08 +0400

    customer table

--- src/ui/customer/customer.tsx
+++ src/ui/customer/customer.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from 'react';
 
 import CustomerHeader from 'ui/customer/customer-header';
-import CustomerTable from 'ui/customer/customer-table';
+import CustomPaginationActionsTable from 'ui/customer/customer-table';
 
 const Customer: FC = () => {
   return (
     <div className="customer-content">
       <CustomerHeader />
-      <CustomerTable />
+      {/* <CustomerTable /> */}
+      <CustomPaginationActionsTable />
     </div>
   );
 };
